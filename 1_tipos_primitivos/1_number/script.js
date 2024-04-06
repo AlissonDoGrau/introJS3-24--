bacaeb76b1697bbf224effec6342491154789195
AlissonDoGrau <127116831+AlissonDoGrau@users.noreply.github.com>
2024-04-05 22:13:54 -0300

    Add files via upload

---
+++ 1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,19 @@
+//tipo number
+
+const meuNumero = 18;
+
+const telefone = 44998779661;
+
+const idade = 16;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+
+console.log(soma);
+
+console.log('Node Funcionou')
---
+++ 3_operadores/script.js
@@ -0,0 +1,14 @@
+//comparação implícita
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+//==compara o valor
+//=== compara o valor e o tipo de dados
+
+// typeof ver o tipo da varivavel
+
+console.log(typeof numero);
+console.log(typeof texto);
